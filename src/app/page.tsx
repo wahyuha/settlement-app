1c262d4476a72a0aa6f83b6e20161ecd6a3a0a16
Wahyu Haryanto <way3haryanto@gmail.com>
2025-11-11 14:04:33 +0700

    feat: first styling for uploader and balance section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,39 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        Hello world
-      </main>
-    </div>
+    <main className={styles.main}>
+      <header className={styles.header}>
+        <h1 className={styles.title}>Settlement Dashboard</h1>
+        <p className={styles.subtitle}>Upload CSV files</p>
+      </header>
+
+      {/* Uploader section */}
+      <div className={styles.container}>
+        <div className={styles.topSection}>
+          <div className={styles.containerUploader}>
+            <div className={styles.uploadArea}>
+              <input
+                type="file"
+                accept=".csv"
+                className={styles.fileInput}
+              />
+              <button
+                className={styles.uploadButton}
+              >
+                Choose CSV File
+              </button>
+            </div>
+          </div>
+
+          <div className={styles.containerBalance}>
+            <h2 className={styles.titleBalance}>Balance</h2>
+            <div className={styles.balanceValue}>
+              Rp 10.000.000
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
